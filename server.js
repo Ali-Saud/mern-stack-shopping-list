e216ac4912f87df4d28142b8473b4d41e4ce8fe2
Ali-Saud <ali_saoud2000@yahoo.com>
2021-11-11 18:25:58 +0200

    version 1.0

--- server.js
+++ server.js
@@ -20,9 +20,9 @@ mongoose.connect(db)
 app.use('/api/items', items);
 
 
-// Serve static assets if in production
+// Serve static assets if in production:
 if (process.env.NODE_ENV === 'production') {
-// Set Static folder
+// Set Static folder:
     app.use(express.static('client/build'));
     app.get('*', (req,res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
