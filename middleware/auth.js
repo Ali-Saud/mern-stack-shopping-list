f93913e1419918b12a46827b6df8dda099830286
Ali-Saud <ali_saoud2000@yahoo.com>
2021-11-12 22:47:53 +0200

    Adding Reducers & Actions to the State

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,7 +5,7 @@ function auth(req, res, next) {
     const token = req.header('x-auth-token');
 
     // Check for Token
-    if(!token) res.status(401).json({ msg: "No token, Authorization denied" });
+    if(!token) return res.status(401).json({ msg: "No token, Authorization denied" });
 
     try {
       // Verify Token
